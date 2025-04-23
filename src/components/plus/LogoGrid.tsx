c3f451e4b0363cb1d8c641cbe94be8317d7a5b34
Axel Cordova <axelsomerseth@gmail.com>
2025-04-23 03:49:33 -0600

    added content on experience page

--- src/components/plus/LogoGrid.tsx
+++ src/components/plus/LogoGrid.tsx
@@ -1,48 +1,98 @@
-export default function LogoGrid() {
+import React from "react";
+import AgileLogo from "../../assets/images/companies/agile_solutions_logo.jpeg";
+import DeunaLogo from "../../assets/images/companies/deuna_logo.png";
+import VarsityDevLogo from "../../assets/images/companies/varsity_dev_logo.png";
+import ContolloLogo from "../../assets/images/companies/contollo_consulting_logo.jpeg";
+
+interface Logo {
+  name: string;
+  source: string;
+  link: string;
+}
+
+const logos: Logo[] = [
+  {
+    name: "Agile Solutions",
+    source: AgileLogo,
+    link: "https://agilesolutions.com/en/",
+  },
+  {
+    name: "DEUNA",
+    source: DeunaLogo,
+    link: "https://www.deuna.com/",
+  },
+  {
+    name: "Varsity Dev",
+    source: VarsityDevLogo,
+    link: "https://varsity.dev/",
+  },
+  {
+    name: "Contollo Consulting",
+    source: ContolloLogo,
+    link: "https://www.contollo.net/",
+  },
+];
+
+const LogoGrid: React.FC = () => {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="theme py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg/8 font-semibold text-gray-900">
-          Trusted by the world’s most innovative teams
+        <h2 className="text-center text-lg/8 font-semibold">
+          Companies that I worked for
         </h2>
-        <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-          <img
-            alt="Transistor"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/transistor-logo-gray-900.svg"
+        <div
+          className={`mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-4 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-4`}
+        >
+          {logos.map((l) => {
+            return (
+              <a
+                href={l.link}
+                className="flex justify-center items-center col-span-2 lg:col-span-1"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  alt={l.name}
+                  src={l.source}
+                  width={140}
+                  height={70}
+                  className="max-h-12 object-contain"
+                />
+              </a>
+            );
+          })}
+          {/* <img
+            alt="Deuna"
+            src={DeunaLogo}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Reform"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/reform-logo-gray-900.svg"
+          /> */}
+          {/* <img
+            alt="Varsity Dev"
+            src={VarsityDevLogo}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="Tuple"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/tuple-logo-gray-900.svg"
-            width={158}
-            height={48}
-            className="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
-          />
-          <img
-            alt="SavvyCal"
-            src="https://tailwindcss.com/plus-assets/img/logos/158x48/savvycal-logo-gray-900.svg"
+          /> */}
+          {/* <img
+            alt="Contollo"
+            src={ContolloLogo}
             width={158}
             height={48}
             className="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
-          />
-          <img
+          /> */}
+          {/* <img
             alt="Statamic"
             src="https://tailwindcss.com/plus-assets/img/logos/158x48/statamic-logo-gray-900.svg"
             width={158}
             height={48}
             className="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
-          />
+          /> */}
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default LogoGrid;
--- src/routes/Experience.tsx
+++ src/routes/Experience.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import Banner from "../components/plus/Banner";
 import LogoGrid from "../components/plus/LogoGrid";
 // import CenteredGrid from "../components/plus/CenteredGrid";
-import Testimonials from "../components/plus/Testimonials";
+// import Testimonials from "../components/plus/Testimonials";
 
 const Experience: React.FC<{}> = () => {
   return (
     <>
       <Banner />
-      <h1 className="w-full flex justify-center items-center">Experience</h1>
+      {/* <h1 className="w-full flex justify-center items-center">Experience</h1> */}
 
       {/* Roles that I have been working in */}
       <LogoGrid />
@@ -16,8 +16,8 @@ const Experience: React.FC<{}> = () => {
       {/* Roles that I have been working in */}
       {/* <CenteredGrid /> */}
 
-      {/* Contact people for recommendations */}
-      <Testimonials />
+      {/* TODO: Contact people for recommendations */}
+      {/* <Testimonials /> */}
     </>
   );
 };
